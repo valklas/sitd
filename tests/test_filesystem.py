6772b83d459ce0932b863a7a09a5bfd1b086ca27
nka <novalkatlas@gmail.com>
2026-09-23 01:19:57 +0800

    Added the first basic test

---
+++ tests/test_filesystem.py
@@ -0,0 +1,8 @@
+from sitd.filesystem import inspect_dir
+from pathlib import Path
+
+source_path = Path("~/.local/share/sitd/storage/").expanduser()
+
+result = inspect_dir(source_path)
+
+assert isinstance(result, list)
