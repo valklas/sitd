08afe08255a86660acb5637b13840b49d7c8d9b0
nka <novalkatlas@gmail.com>
2026-09-23 17:43:07 +0800

    Improved the testing

--- tests/test_filesystem.py
+++ tests/test_filesystem.py
@@ -1,12 +1,24 @@
-from sitd.filesystem import inspect_dir
 from pathlib import Path
+from sitd.filesystem import inspect_dir
+from tempfile import TemporaryDirectory
 
-source_path = Path("~/.local/share/sitd/storage").expanduser()
 
-result = inspect_dir(source_path)
+def create_test_tree(current_dir, depth):
+        files = ["1.txt", "2.txt", "3.txt"]
+    
+        for file_name in files:
+            new_file = current_dir / file_name
+            new_file.write_text(f"This is the {file_name} file.")
+            print(new_file)
+        
+        if depth == 0:
+            return
+    
+        new_dir = current_dir / f"{depth}_dir"
+        new_dir.mkdir()
+        print(new_dir)
 
-assert isinstance(result, list)
-assert len(result) >= 0
+        create_test_tree(new_dir , depth - 1)
 
 
 def check_entries(entries):
@@ -27,4 +39,17 @@ def check_entries(entries):
             check_entries(item["children"])
 
 
-check_entries(result)
+with TemporaryDirectory() as temp_dir:
+    root_dir = Path(temp_dir)
+
+    create_test_tree(root_dir, 5)
+    print(list(root_dir.iterdir()))
+
+    result = inspect_dir(root_dir)
+    
+    assert isinstance(result, list)
+    assert len(result) == 4
+
+    check_entries(result)
+
+    print(result)
