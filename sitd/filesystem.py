0a2c219443af60f90a8b0e63c8ce7ce586331aa0
nka <novalkatlas@gmail.com>
2026-09-23 18:37:06 +0800

    added relative path to the root path

--- sitd/filesystem.py
+++ sitd/filesystem.py
@@ -1,21 +1,22 @@
 from pathlib import Path
 
 
-def inspect_dir(path):
+def inspect_dir(path, root_path):
     entries = []
 
     for item in path.iterdir():
         name = item.name
+        relative_path = item.relative_to(root_path)
 
         if item.is_file():
             file_size_in_bytes = item.stat().st_size
 
-            file_entry = {"name": name, "type": "file", "path": str(item), "file_size": file_size_in_bytes}
+            file_entry = {"name": name, "type": "file", "path": str(relative_path), "file_size": file_size_in_bytes}
             entries.append(file_entry)
 
         elif item.is_dir():
-            dir_entry = {"name": name, "type": "directory", "path": str(item)}
-            children = inspect_dir(item)
+            dir_entry = {"name": name, "type": "directory", "path": str(relative_path)}
+            children = inspect_dir(item, root_path)
             dir_entry["children"] = children
             entries.append(dir_entry)
 
