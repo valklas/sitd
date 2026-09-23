1356da54a77d265e09577fa9d15113b153a05b45
nka <novalkatlas@gmail.com>
2026-09-23 21:05:53 +0800

    updated the /api/files/{path:path} endpoint

--- sitd/server.py
+++ sitd/server.py
@@ -19,8 +19,19 @@ def files():
     return result
 
 
-@app.get("/api/files/{path}")
+@app.get("/api/files/{path:path}")
 def files(path: str):
     sub_path = storage_path / path
-    result = inspect_dir(sub_path, storage_path)
-    return result
+
+    if sub_path.is_dir():
+        result = inspect_dir(sub_path, storage_path)
+        return result
+
+    elif sub_path.is_file():
+        file_entry = {
+            "name": sub_path.name,
+            "type": "file",
+            "path": str(sub_path.relative_to(storage_path)),
+            "file_size": sub_path.stat().st_size
+        }
+        return file_entry
